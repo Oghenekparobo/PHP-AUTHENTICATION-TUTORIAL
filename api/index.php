a8697bded6a47a6bdc33b2a603dc37889489e587
oghenekparobo <classicsteph11@gmail.com>
2024-02-26 19:41:37 +0100

    updated template

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
+require dirname(__DIR__)  . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+set_error_handler('ErrorHandler::handleError');
+set_exception_handler('ErrorHandler::handleException');
 
 
-echo 'php authentication tutorial';
+
+header("Content-type: application/json; charset=UTF-8");
+
+echo $_ENV["DB_HOST"] . " ";
+echo $_ENV["DB_NAME"] . " ";
+echo $_ENV["DB_USER"] . " ";
+echo $_ENV["DB_PASS"];
