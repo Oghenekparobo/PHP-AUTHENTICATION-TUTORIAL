a52a6eda493fa3850c0149d72ff0e20e82f4b329
oghenekparobo <classicsteph11@gmail.com>
2024-03-07 13:38:06 +0100

    edit register.php

--- register.php
+++ register.php
@@ -3,45 +3,49 @@
 require __DIR__ . "/vendor/autoload.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
+
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
-    
-    $database = new Database($_ENV["DB_HOST"],
-                             $_ENV["DB_NAME"],
-                             $_ENV["DB_USER"],
-                             $_ENV["DB_PASS"]);
-                             
+
+    $database = new Database(
+        $_ENV["DB_HOST"],
+        $_ENV["DB_NAME"],
+        $_ENV["DB_USER"],
+        $_ENV["DB_PASS"]
+    );
+
     $conn = $database->getConnection();
-    
-    $sql = "INSERT INTO user (name, username, password_hash, api_key)
-            VALUES (:name, :username, :password_hash, :api_key)";
-            
+
+    $sql = "INSERT INTO user (name, username, password_hash)
+            VALUES (:name, :username, :password_hash)";
+
     $stmt = $conn->prepare($sql);
-    
+
     $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
-    $api_key = bin2hex(random_bytes(16));
-    
+
+
     $stmt->bindValue(":name", $_POST["name"], PDO::PARAM_STR);
     $stmt->bindValue(":username", $_POST["username"], PDO::PARAM_STR);
     $stmt->bindValue(":password_hash", $password_hash, PDO::PARAM_STR);
-    $stmt->bindValue(":api_key", $api_key, PDO::PARAM_STR);
-    
+
+
     $stmt->execute();
-    
-    echo "Thank you for registering. Your API key is ", $api_key;
+
+    echo "Thank you for registering.";
     exit;
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Registration</title>
-   <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="container">
         <h2>User Registration</h2>
@@ -64,4 +68,5 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         </form>
     </div>
 </body>
+
 </html>
